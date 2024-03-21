3d4ef1c5f0f693f437c2e41ad1728edf5c2ba0e4
Adam Chimera <adam@lightningjar.com>
2024-03-21 00:11:26 -0400

    Refactor Ex30ButtonEat component styles

--- src/lib/components/atoms/Ex30ButtonEat.svelte
+++ src/lib/components/atoms/Ex30ButtonEat.svelte
@@ -6,8 +6,11 @@
 </script>
 
 <template lang="pug">
-	button.rounded-lg.w-36(on:click!="{ onClick }")
-		img.h-14.m-auto.mb-1(
+	button.rounded-lg.w-44.border.border-eerie.py-1.px-4.flex.flex-col.items-center(
+		class="hover:shadow-md hover:shadow-blue-400",
+		on:click!="{ onClick }"
+	)
+		img.h-14(
 			alt="A cartoon mouth",
 			src!="{ buttonImage }"
 		)
