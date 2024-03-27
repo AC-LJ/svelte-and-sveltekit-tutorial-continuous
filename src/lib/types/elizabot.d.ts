c3f21c84b2ca5facb65e64c6ef2c985af1f930f9
Adam Chimera <adam@lightningjar.com>
2024-03-27 17:43:03 -0400

    Remove commented out code in elizabot.d.ts and types.ts

--- src/lib/types/elizabot.d.ts
+++ src/lib/types/elizabot.d.ts
@@ -1,10 +1,1 @@
-// declare module 'elizabot' {
-//     export class ElizaBot {
-//       constructor();
-//       getInitial(): string;
-//       transform(input: string): string;
-//       // Add other methods and properties as needed
-//     }
-//   }
-
 declare module 'elizabot'
--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,11 +1,2 @@
 // a great place to put some custom types
 
-// declare module 'elizabot' {
-//   export class ElizaBot {
-//     constructor();
-//     getInitial(): string;
-//     transform(input: string): string;
-//     // Add other methods and properties as needed
-//   }
-// }
-
