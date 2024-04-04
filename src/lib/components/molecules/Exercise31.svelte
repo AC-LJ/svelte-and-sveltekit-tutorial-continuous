c8fe0cfb32ff3e779c7bf194465835e45621330d
Adam Chimera <adam@lightningjar.com>
2024-04-04 18:18:57 -0400

    Refactor Exercise31.svelte to use external library for markdown parsing

--- src/lib/components/molecules/Exercise31.svelte
+++ src/lib/components/molecules/Exercise31.svelte
@@ -1,145 +1,13 @@
 <script lang="ts">
+	// external libraries
+	import { marked } from "marked";
+
 	// components
 	import ExerciseHeader from "$atoms/ExerciseHeader.svelte";
 	import { get } from "svelte/store";
 
 	// variables
 	let value = `Some words are *italic*, some are ***bold***\n\n- lists\n- are\n- cool`;
-
-	// functions
-	function marked(text: string) {
-		// function-scoped variables
-
-		// helper functions
-
-		function getTermIndices(stringToSearch: string, searchTerm: string) {
-			const searchFrame = searchTerm.length;
-
-			const termIndices = [];
-			let bypass = 0;
-
-			for (let i = 0; i < stringToSearch.length; i++) {
-				if (bypass > 0) {
-					bypass--;
-				} else if (stringToSearch.slice(i, i + searchFrame) === searchTerm) {
-					bypass = searchFrame - 1;
-					termIndices.push(i);
-				}
-			}
-			return termIndices;
-		}
-
-		function performReplacement(
-			replacement: string,
-			endPartOne: number,
-			startPartTwo: number,
-		) {
-			const sliceOne = text.slice(0, endPartOne);
-			const sliceTwo = text.slice(startPartTwo);
-
-			text = sliceOne + replacement + sliceTwo;
-		}
-
-		function parseEmAndStrong() {
-			const localReplacementKey: Record<string, string> = {
-				// Order of keys is important here. Otherwise, the ** will be interpreted as two *'s
-				"**": "strong",
-				"*": "em",
-			};
-
-			let replacement: string;
-
-			for (let keyTerm of Object.keys(localReplacementKey)) {
-				const termIndices = getTermIndices(text, keyTerm);
-
-				if (termIndices.length % 2 !== 0) {
-					termIndices.pop();
-				}
-
-				let seekingOpener = true;
-
-				// running displacement tracks the change in length when a search term is replaced with an HTML tag
-				let runningDisplacement = 0;
-
-				for (let index of termIndices) {
-					if (seekingOpener) {
-						replacement = "<" + localReplacementKey[keyTerm] + ">";
-					} else {
-						replacement = "</" + localReplacementKey[keyTerm] + ">";
-					}
-
-					performReplacement(
-						replacement,
-						index + runningDisplacement,
-						index + keyTerm.length + runningDisplacement,
-					);
-
-					runningDisplacement += replacement.length - keyTerm.length;
-					seekingOpener = !seekingOpener;
-				}
-			}
-
-			return;
-		}
-
-		function parseLists() {
-			const localReplacementKey: Record<string, string> = {
-				"\n-": "li",
-				"\n": "ul",
-			};
-
-			let replacement = "";
-
-			// First place the list item tags
-			const itemTermIndices = getTermIndices(text, "\n-");
-			let runningDisplacement = 0;
-
-			for (let index of itemTermIndices) {
-				if (itemTermIndices.indexOf(index) === 0) {
-					replacement = "<" + localReplacementKey["\n-"] + ">";
-				} else {
-					replacement =
-						"</" +
-						localReplacementKey["\n-"] +
-						"><" +
-						localReplacementKey["\n-"] +
-						">";
-				}
-				performReplacement(
-					replacement,
-					index + runningDisplacement,
-					index + "\n-".length + runningDisplacement,
-				);
-
-				runningDisplacement += replacement.length - "\n-".length;
-			}
-
-			// Then place the unordered list tags (this is totally ad hock)
-			runningDisplacement = 0;
-
-			if (
-				(!text.includes("<ul>") && text.includes("<li>")) ||
-				text.indexOf("<ul>") > text.lastIndexOf("<li>")
-			) {
-				performReplacement("<ul>", text.indexOf("<li>"), text.indexOf("<li>"));
-				runningDisplacement += "<ul>".length;
-			}
-
-			if (
-				!text.includes("</ul>") ||
-				text.lastIndexOf("<ul>") > text.lastIndexOf("</ul>")
-			) {
-				text += "</li></ul>";
-			}
-		}
-
-		function parseParagraphs() {}
-
-		// execution sequence
-		parseEmAndStrong();
-		parseLists();
-		return text;
-	}
 </script>
 
 <template lang="pug">
@@ -154,9 +22,22 @@
 		textarea.flex.flex-grow(bind:value!="{ value }")
 
 		p output
-		div {  @html marked(value)  }</template>
+		div {  @html marked.parse(value)  }</template>
 
 <style>
+	:global(li) {
+		position: relative;
+		margin-left: 0.9em;
+	}
+
+	:global(li::before) {
+		position: absolute;
+		content: "•";
+		font-size: 1.75em;
+		line-height: 1rem;
+		left: -1.15rem;
+		top: 0.1rem;
+	}
 	.grid {
 		display: grid;
 		grid-template-columns: 5em 1fr;
@@ -164,7 +45,6 @@
 		grid-gap: 1em;
 		height: 100%;
 	}
-
 	textarea {
 		flex: 1;
 		resize: none;
