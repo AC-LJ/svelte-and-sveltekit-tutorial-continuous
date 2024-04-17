43deed93d2a98aeecc7e0e7da03b6f69775f9dba
Adam Chimera <adam@lightningjar.com>
2024-04-16 20:44:06 -0400

    Nomenclature change to match other LJ SvelteKit projects

---
+++ src/lib/components/organisms/GlobalHeader.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	// components
+	import BasicSvelteNav from "$components/organisms/BasicSvelteNav.svelte";
+</script>
+
+<template lang="pug">
+	header.bg-neutral-900.text-blue-300.text-center.px-8
+		h1.text-42.pt-10.pb-5.leading-10(class="md:text-48") The Svelte Tutorial Demonstrated
+		p.text-20.pb-10(class="md:text-24") Completed with Lightning Jar's "Skinnypug" stack
+	BasicSvelteNav</template>
