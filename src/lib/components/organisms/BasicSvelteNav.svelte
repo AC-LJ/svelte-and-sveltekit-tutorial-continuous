812e71a0a4caeaf5c57aa417c79383b346ebad1d
Adam Chimera <adam@lightningjar.com>
2024-04-18 18:31:24 -0400

    Add ButtonNavItem component and update BasicSvelteNav to use it

--- src/lib/components/organisms/BasicSvelteNav.svelte
+++ src/lib/components/organisms/BasicSvelteNav.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { page } from "$app/stores";
-	import { fly } from "svelte/transition";
+	import { fade, fly } from "svelte/transition";
 
 	// components
 	import SvgSectionSelect from "$atoms/SvgSectionSelect.svelte";
+	import SvgHamburger from "$atoms/SvgHamburger.svelte";
+	import SvgCloseSymbol from "$atoms/SvgCloseSymbol.svelte";
+	import ButtonNavItem from "$atoms/ButtonNavItem.svelte";
 
 	// stores
-	import { navIsOpen } from "$stores/navStateStore";
+	import { mobileNavIsOpen } from "$stores/navStateStore";
 	import { pageMode } from "$stores/viewportStore";
 	import { currentPage } from "$stores/paginationStore";
 
 	// variables
-	const sectionTitles: string[] = [
+	const basicSectionTitles: string[] = [
 		"intro",
 		"reactivity",
 		"props",
@@ -23,41 +26,43 @@
 	];
 
 	let selectedIndex = 0;
-	$currentPage = sectionTitles[selectedIndex];
+	$currentPage = basicSectionTitles[selectedIndex];
 
 	// reactive variables
-	$: $navIsOpen = $pageMode !== "narrow" ? false : $navIsOpen;
+	$: $mobileNavIsOpen = $pageMode !== "narrow" ? false : $mobileNavIsOpen;
 
 	// functions
 	function toggleNav() {
-		$navIsOpen = !$navIsOpen;
+		$mobileNavIsOpen = !$mobileNavIsOpen;
 	}
 
 	function previous() {
 		selectedIndex = selectedIndex === 0 ? 0 : selectedIndex - 1;
-		$currentPage = sectionTitles[selectedIndex];
+		$currentPage = basicSectionTitles[selectedIndex];
 	}
 
 	function next() {
 		selectedIndex =
-			selectedIndex === sectionTitles.length - 1
-				? sectionTitles.length - 1
+			selectedIndex === basicSectionTitles.length - 1
+				? basicSectionTitles.length - 1
 				: selectedIndex + 1;
-		$currentPage = sectionTitles[selectedIndex];
+		$currentPage = basicSectionTitles[selectedIndex];
 	}
 
-	function mouseSelectPage(page: string) {
-		selectedIndex = sectionTitles.indexOf(page);
+	function handlePageButtonClick(page: string) {
+		console.log(
+			"onclick function in BasicSvelteNav says: $currentPage is",
+			$currentPage,
+		);
+		selectedIndex = basicSectionTitles.indexOf(page);
 		$currentPage = page;
 	}
 </script>
 
 <template lang="pug">
-	//- .bg-eerie
-	//- 	p $pageMode: { $pageMode }
-	//- 	p $navIsOpen: { $navIsOpen }
-	//- 	p initialPageMode: { initialPageMode }
-	.page-select-container.bg-eerie.sticky.h-16.top-0.z-50
+	nav.box-content.flex.items-center.sticky.h-24.px-8.top-0.z-50.mx-auto.bg-eerie.border-t.border-b-2.border-primary(
+		class="max-w-[1800px]"
+	)
 		+if('["desktop", "grid-cols-2"].includes($pageMode)')
 			.flex.gap-1.justify-around.items-end.px-8.py-3
 				button.flex.justify-center.items-center(
@@ -66,34 +71,36 @@
 				)
 					SvgSectionSelect(direction="left")
 
-				+each('sectionTitles as page')
+				+each('basicSectionTitles as page')
 					div(class="lg:w-[104px] lg:min-w-[104px] lg:h-10 lg:border-2 lg:border-blue-300")
-						p.text-18.text-center(
-							on:click!="{ () => mouseSelectPage(page) }",
+						button.text-18.text-center(
+							on:click!="{ (page) => handlePageButtonClick }",
 							style!="{ $currentPage === page ? 'font-weight: 800; text-shadow: #93c5fd 1px 0 16px; font-size: 20px;' : 'font-weight: 200;' }"
 						) { page }
 
 				button.flex.justify-center.items-center(
 					on:click!="{ next }",
-					style!="{ selectedIndex < sectionTitles.length - 1 ? '' : 'visibility: hidden;' }"
+					style!="{ selectedIndex < basicSectionTitles.length - 1 ? '' : 'visibility: hidden;' }"
 				)
 					SvgSectionSelect(direction="right")
 			+else
-				+if('!$navIsOpen')
-					button.absolute.flex.w-32.justify-around.items-center.px-6.py-4(
-						on:click!="{ () => toggleNav() }"
-					) Menu
-						SvgSectionSelect(direction="right")
+				+if('!$mobileNavIsOpen')
+					button.flex.justify-center.items-center.p-3.border.border-primary.rounded-lg(
+						on:click!="{ () => toggleNav() }",
+						transition:fly!="{{ x: -500, duration: 500 }}"
+					)
+						SvgHamburger
 					+else
-						.w-60.bg-eerie.flex.flex-col.p-8(
-							transition:fly!="{{ x: -200, duration: 500 }}"
+						.h-fit.w-full.flex.flex-wrap.justify-center.items-center.gap-2(
+							transition:fly!="{{ x: 500, duration: 500 }}"
 						)
-							+each('sectionTitles as page')
-								button.absolute.top-0.right-0.flex.justify-center.items-center.w-8.h-8(
-									on:click!="{ () => $navIsOpen = false }"
-								) x
-								div(class="lg:w-[104px] lg:min-w-[104px] lg:h-10 lg:border-2 lg:border-blue-300")
-									p.text-18.text-center(
-										on:click!="{ () => mouseSelectPage(page) }",
-										style!="{ $currentPage === page ? 'font-weight: 700; text-shadow: #93c5fd 1px 0 16px; font-size: 20px;' : 'font-weight: 200;' }"
-									) { page }</template>
+							+each('basicSectionTitles as page')
+								ButtonNavItem(
+									onClick!="{ () => handlePageButtonClick(page) }",
+									page!="{ page }"
+								)
+
+							button.absolute.top-2.right-3.flex.justify-center.p-1.border-2.border-primary.rounded-lg(
+								on:click!="{ () => ($mobileNavIsOpen = false) }"
+							) 
+								SvgCloseSymbol</template>
