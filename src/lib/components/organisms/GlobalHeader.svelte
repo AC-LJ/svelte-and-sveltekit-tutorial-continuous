f9ed0e9eb474bd85f37f7f0986576f00ad10e741
Adam Chimera <adam@lightningjar.com>
2024-04-18 18:31:08 -0400

    Update GlobalHeader styles and class names

--- src/lib/components/organisms/GlobalHeader.svelte
+++ src/lib/components/organisms/GlobalHeader.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <template lang="pug">
-	header.bg-neutral-900.text-blue-300.text-center.px-8
-		h1.text-42.pt-10.pb-5.leading-10(class="md:text-48") The Svelte Tutorial Demonstrated
-		p.text-20.pb-10(class="md:text-24") Completed with Lightning Jar's "Skinnypug" stack
+	header.bg-eerie.text-primary.text-center.px-8.py-10.mx-auto(class="max-w-[1800px]")
+		h1.text-30.leading-10(class="sm:text-36 md:text-48") The Svelte Tutorial Demonstrated
+		p.mt-8.text-20(class="md:text-24") Completed with Lightning Jar's "Skinnypug" stack
 	BasicSvelteNav</template>
