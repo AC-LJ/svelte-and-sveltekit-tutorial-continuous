0c79cada55e7402a13419bb8339ce966666fd46e
Adam Chimera <adam@lightningjar.com>
2024-04-18 18:30:40 -0400

    Add SvgCloseSymbol.svelte and SvgHamburger.svelte components

---
+++ src/lib/components/atoms/SvgCloseSymbol.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<template lang="pug">
+	svg(
+		fill="none",
+		height="16",
+		viewBox="0 0 30 30",
+		width="16",
+		xmlns="http://www.w3.org/2000/svg"
+	)
+		line(
+			stroke="#93c5fd",
+			stroke-linecap="round",
+			stroke-width="4",
+			x1="5",
+			x2="25",
+			y1="5",
+			y2="25"
+		)
+		line(
+			stroke="#93c5fd",
+			stroke-linecap="round",
+			stroke-width="4",
+			x1="25",
+			x2="5",
+			y1="5",
+			y2="25"
+		)</template>
---
+++ src/lib/components/atoms/SvgHamburger.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<template lang="pug">
+	svg(
+		fill="none",
+		height="27",
+		viewBox="0 0 40 30",
+		width="40",
+		xmlns="http://www.w3.org/2000/svg"
+	)
+		rect(
+			fill="#93c5fd",
+			height="3",
+			width="40"
+		)
+		rect(
+			fill="#93c5fd",
+			height="3",
+			width="40",
+			y="12.5"
+		)
+		rect(
+			fill="#93c5fd",
+			height="3",
+			width="40",
+			y="25"
+		)</template>
