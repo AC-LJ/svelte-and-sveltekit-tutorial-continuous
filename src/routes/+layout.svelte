b3c85c85c49bc76610b4d60325a71c3f7efa7f03
Adam Chimera <adam@lightningjar.com>
2024-04-24 09:23:35 -0400

    Refactor styles and dimensions in Ex30ButtonEat.svelte, add neon font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,14 @@
 
 <template lang="pug">
 	slot</template>
+
+<style>
+	@font-face {
+		font-family: "neonderthawregular";
+		src:
+			url("src/lib/assets/neonderthaw-regular-webfont.woff2") format("woff2"),
+			url("src/lib/assets/neonderthaw-regular-webfont.woff") format("woff");
+		font-weight: normal;
+		font-style: normal;
+	}
+</style>
